832099330c88837a41675bd0792c405f49976f4e
SongJunHyuk <83599564+FE-Nerdy@users.noreply.github.com>
2025-08-07 20:15:28 +0900

    Feat: addTask 기능 추가
    task를 To do list에 추가하기 기능 구현

--- js/script.js
+++ js/script.js
@@ -1,42 +1,46 @@
-const inputBox = document.getElementById('inputBox');
-const addBtn   = document.getElementById('addBtn');
-const todoList = document.getElementById('todoList');
-const doneList = document.getElementById('doneList');
+const inputBox = document.getElementById("inputBox");
+const addBtn = document.getElementById("addBtn");
+// const todoList = document.getElementById("todoList");
+// const doneList = document.getElementById("doneList");
 
 function addTask() {
-    if(inputBox.value.trim() === '') {
-        alert('Please enter a task.');
-    }
-    else {
-        let li = document.createElement('li');
-        li.textContent = inputBox.value;
-        todoList.appendChild(li);
-        
-    }
-    inputBox.value = '';
+  const newTask = inputBox.value.trim();
+
+  if (inputBox.value.trim() === "") {
+    alert("Please enter a task.");
+  } else {
+    const li = document.createElement("li");
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.checked = false;
+
+    const span = document.createElement("span");
+    span.textContent = newTask;
+
+    li.appendChild(check);
+    li.appendChild(span);
+    todoList.appendChild(li);
+  }
+  inputBox.value = "";
 }
 
-// addBtn.addEventListener('click', () => {
-//   const task = inputBox.value.trim();
-//   if (!task) return;
-//   tasks.push({ id: Date.now(), text: txt, done: false, memo: '' });
-//   newTask.value = '';
-//   render();
-// });
+addBtn.addEventListener("click", () => {
+  addTask();
+});
 
 // 엔터키로도 추가
-newTask.addEventListener('keydown', e => {
-  if (e.key === 'Enter') addBtn.click();
+inputBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") addBtn.click();
 });
 
-todoList.addEventListener('click', function(e) {
-    if(e.target.tagName === 'LI') {
-        let li = e.target;
-        doneList.appendChild(li);
-        li.style.textDecoration = 'line-through';
-    }
-});
+// todoList.addEventListener("click", function (e) {
+//   if (e.target.tagName === "LI") {
+//     let li = e.target;
+//     doneList.appendChild(li);
+//     li.style.textDecoration = "line-through";
+//   }
+// });
 
-function saveData() {
-    localStorage.setItem('todoList', todoList.innerHTML);
-}
+// function saveData() {
+//   localStorage.setItem("todoList", todoList.innerHTML);
+// }
